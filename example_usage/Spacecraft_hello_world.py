939a15fdece4517a2eed4ee4f22fb988e28eb730
Michael Gerits <michael.gerits@alchemich.be>
2024-07-30 10:14:48 +0200

    Added comments to functions for better understanding

--- example_usage/Spacecraft_hello_world.py
+++ example_usage/Spacecraft_hello_world.py
@@ -9,15 +9,20 @@ Two-body propagation with J2 perturbation for 100 periods
 
 # Python standard libraries
 # AWP libraries
+
+
 from Spacecraft import Spacecraft as SC
 from planetary_data import earth
 
 if __name__ == '__main__':
+	# coes = [semi-major axis(km) ,eccentricity ,inclination (deg) , , ,]
 	coes = [ earth[ 'radius' ] + 1000, 0.05, 30.0, 0.0, 0.0, 0.0 ]
 	sc   = SC(
 			{
 			'coes'       : coes,
-			'tspan'      : '100', 
+			#Tspan is either the amount or seconds. 
+			#If it is a string,it is the amount of orbits
+			'tspan'      : '1', 
 			'dt'         : 100.0,
 			'orbit_perts': { 'J2': True }
 			} )
